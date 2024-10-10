75f5cf77e95b6ebebaed102bee40cfd2e63ed86d
dagimtsegaw <dagatse577@gmail.com>
2024-10-10 17:38:22 +0300

    bg css

--- src/Components/Background/Background.js
+++ src/Components/Background/Background.js
@@ -6,18 +6,18 @@ import fallbackImage from "../../assets/fallback-image.png";
 const Background = () => {
   return (
     <>
-      <div>
-        <video
-          playsInline
-          autoPlay
-          muted
-          loop
-          preload="auto"
-          poster={fallbackImage}
-        >
-          <source src={video} type="video?mp4"></source>
-        </video>
-      </div>
+      <div className="shadow-overlay"></div>
+      <video
+        playsInline
+        autoPlay
+        muted
+        loop
+        preload="auto"
+        poster={fallbackImage}
+        id="bg"
+      >
+        <source src={video} type="video?mp4"></source>
+      </video>
     </>
   );
 };
