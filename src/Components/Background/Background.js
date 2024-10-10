5e91756ecfb9f9c9078b7845525f0eeba4c6824e
dagimtsegaw <dagatse577@gmail.com>
2024-10-10 17:25:00 +0300

    importing background necceray files

---
+++ src/Components/Background/Background.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./background.css";
+import video from "../../assets/bg-universe.mp4";
+import fallbackImage from "../../fallback-image.pngassets/";
+
+const Background = () => {
+  return <div>Background</div>;
+};
+
+export default Background;
