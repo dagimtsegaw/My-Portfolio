c2f4b8caaed75a4eea22e3252ab57d761e0110ba
dagimtsegaw <dagatse577@gmail.com>
2024-10-12 17:19:15 +0300

    about subheading and about menu item jsx

--- src/Pages/About/AboutSubheading.js
+++ src/Pages/About/AboutSubheading.js
@@ -1,7 +1,18 @@
-import React, { Component } from "react";
+import React from "react";
+import classNames from "classnames";
+import "./abotMenu.css";
 
-export default class AboutSubheading extends Component {
-  render() {
-    return <div>AboutSubheading</div>;
-  }
-}
+const AboutSubheading = (title, onClick, active, content, menuItem) => {
+  const subContainerClass = `subcontainer-${menuItem}`;
+  return (
+    <div
+      className={classNames({ "active-subheading": active })}
+      onClick={onClick}
+    >
+      <h3 onClick={onClick}>{title}</h3>
+      <div className="p-contianer">{content}</div>
+    </div>
+  );
+};
+
+export default AboutSubheading;
