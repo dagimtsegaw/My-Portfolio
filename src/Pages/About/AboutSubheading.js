b0d4cbf8a004dc1cae483680b834f16115f14a0c
dagimtsegaw <dagatse577@gmail.com>
2024-10-17 19:29:42 +0300

    added comment

--- src/Pages/About/AboutSubheading.js
+++ src/Pages/About/AboutSubheading.js
@@ -4,6 +4,7 @@ import "./abotMenu.css";
 
 const AboutSubheading = ({ title, onClick, active, content, menuItem }) => {
   const subContainerClass = `subcontainer-${menuItem}`;
+  //the return that will be rendered on the page
   return (
     <div
       className={classNames({ "active-subheading": active })}
