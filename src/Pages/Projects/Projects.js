c277c076409abe65d61a81bb28f970d375f3c9ee
dagimtsegaw <dagatse577@gmail.com>
2024-10-19 16:49:57 +0300

    access project menu comment

--- src/Pages/Projects/Projects.js
+++ src/Pages/Projects/Projects.js
@@ -3,6 +3,7 @@ import ProjectsMenu from "./ProjectsMenu";
 
 export default class Projects extends Component {
   render() {
+    //acess the project menu component
     return (
       <>
         <ProjectsMenu />;
